695effed13fc5c3ccd272096ac47c43f795cef94
dev-nu11 <orlowski@dev-nu11.de>
2012-10-09 15:39:50 +0200

    First Try

---
+++ main.java
@@ -0,0 +1,12 @@
+import mastermind_gui.*;
+
+public class main {
+	public static void main(String[] args) {
+		final gui g = new gui();
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+                g.createAndShowGUI();
+            }
+        });
+	}
+}
---
+++ mastermind_gui/gui.java
@@ -0,0 +1,19 @@
+package mastermind_gui;
+
+import javax.swing.*; 
+
+public class gui {
+    public static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
