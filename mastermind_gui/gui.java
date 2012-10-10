60fdb81fb282237f09260a8fe4b4da5867a42aaf
h8h <mc2m382@chomeyer.de>
2012-10-10 13:51:42 +0200

    Code Generator funktioniert jetzt perfekt :)

--- mastermind_gui/gui.java
+++ mastermind_gui/gui.java
@@ -6,23 +6,24 @@ import javax.swing.*;
 import java.util.*;
 
 public class gui {
+	JButton[] code;
+	
   public void showGUI() {
 		//Erstellt ein neues Fenster		
 		final JFrame frame = new JFrame("MasterMind PP-1");
+		frame.setPreferredSize(new Dimension(640, 480));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout (new FlowLayout());
 		//Spielbare Farben und Namen ;)
 		final Color[] color_buttons = {Color.GREEN,Color.RED,Color.BLACK,Color.BLUE,Color.MAGENTA,Color.ORANGE,Color.PINK,Color.WHITE,Color.YELLOW,Color.CYAN};
+		init(frame, color_buttons);
 		//Action fuer den ReMixIt - Button
 		JButton mixer = new JButton ("ReMixIt");
+		shufflecolor(color_buttons);
 		ActionListener al = new ActionListener() {
 			public void actionPerformed( ActionEvent e ) {
-				//Neue Zufallsbutton erstellen
-				JButton[] coded_buttons = shufflecolor(color_buttons);
-				//Buttons zum Panel hinzufuegen
-				for(int i=0; i < coded_buttons.length; i++) {
-					frame.getContentPane().add(coded_buttons[i]);
-				}
+				shufflecolor(color_buttons);
+				frame.revalidate();
 			}
 		};
 		//ReMixIt mit Click-Action verbinden
@@ -33,18 +34,21 @@ public class gui {
 		frame.pack();
 		frame.setVisible(true);
 	}
-	
-	private JButton[] shufflecolor(Color[] c) {
+	private void init(JFrame frame, Color[] c) {
 		//Neues Button Array = geheimer Code, also haelfte vom Farben-Array
-		JButton[] coding_buttons = new JButton[c.length/2];
+		code = new JButton[c.length/2];
+		for(int i=0; i < code.length; i++) {
+			code[i] = new JButton("              ");
+			frame.getContentPane().add(code[i]);
+		}		
+	}
+	
+	private void shufflecolor(Color[] c) {
 		Random r = new Random();
 		int randomize_color = 0;
-		for(int i=0; i < coding_buttons.length; i++) {
+		for(int i=0; i < code.length; i++) {
 				randomize_color = r.nextInt(c.length);
-				JButton random_button = new JButton("              ");
-				random_button.setBackground(c[randomize_color]);
-				coding_buttons[i] = random_button;
+				code[i].setBackground(c[randomize_color]);
 		}
-		return coding_buttons;
 	}
 }
