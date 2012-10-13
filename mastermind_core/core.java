a5359e2462b414ec919a915b15cab1a2137e0010
Christian Homeyer <homy@gn8.(none)>
2012-10-13 19:24:39 +0200

    Optimize, use GridBagLayout now

--- mastermind_core/core.java
+++ mastermind_core/core.java
@@ -4,20 +4,33 @@ import java.util.*;
 
 public class core {
 	String[] availableColors =	{"#0000ff","#008000","#00bfff","#483d8b","#696969","#800000","#8b4513","#000000","#FFFFFF","#da70d6"};
+	String[] usedColors;
 	String[] code;
 
 	public core (int codeSize) {
-		code = new String[codeSize];
+		initColors(codeSize);
+	}
+
+	private void initColors(int codeSize) {
+		usedColors = new String[codeSize];
+		code = new String[codeSize / 2];
+		for (int i = 0; i < codeSize; i++) {
+			usedColors[i] = availableColors[i];	
+		}
 	}
 
 	public String[] generateCode() {
 		int randomizeColor = 0;
 		Random r = new Random();
 		for (int i = 0; i < code.length; i++) {
-			randomizeColor = r.nextInt(availableColors.length);		
-			code[i] = availableColors[randomizeColor];
+			randomizeColor = r.nextInt(usedColors.length);		
+			code[i] = usedColors[randomizeColor];
 		}
 		return code;
+	}
+	
+	public String[] getUsedColors() {
+		return usedColors;
 	}
 }
 
--- mastermind_gui/gui.java
+++ mastermind_gui/gui.java
@@ -7,26 +7,69 @@ import mastermind_core.core;
 
 public class gui {
 	JButton[] code;
+	JButton[] usingColors;
 	core mastermindCore;	
+
+// private void addComponent(Container container, Component component, int gridx, int gridy, int gridwidth, int gridheight, int anchor, int fill)
 
   public void showGUI() {
 		//Erstellt ein neues Fenster		
 		JFrame frame = new JFrame("MasterMind PP-1");
 		//frame.setPreferredSize(new Dimension(640, 480));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setContentPane(Box.createHorizontalBox());
+		frame.setLayout(new GridBagLayout());
+
 		//Spielbare Farben und Namen ;)
-		int codeSize = 4;
+
+		int codeSize = 8;
 		mastermindCore = new core(codeSize);
-		code = new JButton[codeSize];
+		usingColors = new JButton[codeSize];
+		code = new JButton[codeSize / 2];
 
 		for(int i=0; i < code.length; i++) {
-			code[i] = new JButton("              ");
-			frame.add(code[i]);
+			code[i] = new JButton();
+			addComponent(frame, code[i], i+i,0,2,1, GridBagConstraints.NORTH, GridBagConstraints.BOTH);
 		}	
+		setBackgroundColor();
+
+		String[] usedColors =	mastermindCore.getUsedColors();
+		for(int i=0; i < usingColors.length; i++) {
+			JButton chooseColors = new JButton ();
+			chooseColors.setBackground(Color.decode(usedColors[i]));
+			usingColors[i] = chooseColors;
+			addComponent(frame, usingColors[i], i,10,1,1, GridBagConstraints.NORTH, GridBagConstraints.BOTH);
+		}
+
+		JMenuBar bar = new JMenuBar();
+		{
+			JMenu menu = new JMenu("File");
+			{
+			JMenuItem item = new JMenuItem("Neues Spiel");
+			item.addActionListener(new ActionListener () {
+					public void actionPerformed (ActionEvent e) {
+						setBackgroundColor();
+					}
+			});
+			menu.add(item);
+			}
+			bar.add(menu);
+		}
+
+
+		frame.setJMenuBar(bar);
+		//addComponent(frame, new JButton("BLA"), 15, 4, 1, 1, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL);
+		/*/pane.setLayout(new GridBagLayout());
+		Box hbox = Box.createHorizontalBox();
+		Box hbox1 = Box.createHorizontalBox();
+		
+
+		
+
+
+
 		//Action fuer den ReMixIt - Button
 		JButton mixer = new JButton ("ReMixIt");
-		setBackgroundColor();
+
 		//ReMixIt mit Click-Action verbinden
 		mixer.addActionListener(new ActionListener() {
 			public void actionPerformed( ActionEvent e ) {
@@ -34,12 +77,27 @@ public class gui {
 			}
 		});
 		//ReMixIt-Button zum Panel hinzufuegen
-		frame.add(Box.createHorizontalStrut(50));
-		frame.add(mixer);
+		hbox1.add(mixer);
+		Box vbox = Box.createVerticalBox();
+		{
+			//vbox.add(hbox);
+			//vbox.add(pane);
+			//vbox.add(hbox1);	    
+		}
+
+		//
+		//frame.add(vbox);
 		//Display the window.
+		*/
+		//frame.add(pane);
 		frame.pack();
 		frame.setVisible(true);
 	}
+
+  private void addComponent(Container container, Component component, int gridx, int gridy, int gridwidth, int gridheight, int anchor, int fill) {
+    GridBagConstraints gbc = new GridBagConstraints(gridx, gridy, gridwidth, gridheight, 1.0, 1.0, anchor, fill, new Insets(0, 0, 0, 0), 0, 0);
+    container.add(component, gbc);
+  }
 
 	private void setBackgroundColor() {
 		String [] colorCode = mastermindCore.generateCode();
