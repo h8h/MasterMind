9f6bf5f9523fdc7d573b4f26242118c6ec9f62ec
Stefan Schmid <stefan.schmid5@t-online.de>
2012-10-16 11:26:09 +0200

    Check-Funktion zum setzten der entsprechenden Stifte nach jedem Zug... Schleifen sind noch nicht optimal, vielleicht Rekursion??
    FUNKTIONSFÄHIG

--- mastermind_core/core.java
+++ mastermind_core/core.java
@@ -32,6 +32,44 @@ public class core {
 	public String[] getUsedColors() {
 		return usedColors;
 	}
-}
+        
+        public static String[] color_check (String[] codeColor, String[] userColor){
+        
+            String[] codeColors = codeColor;
+            String[] userColors = userColor;
+        
+            String[] check = new String[codeColors.length];
+            int checkpos = 0;
+        
+            // Auf Farbe+Position prüfen
+            for(int i=0 ; i<userColors.length ; i++){
+                if(userColors[i].equals(codeColors[i])){
+                    check[checkpos++] = "X";
+                    userColors[i] = "UX";
+                    codeColors[i] = "CX";
+                }
+            }
+
+            // Nur auf Farbe prüfen
+            loop1: for(int i=0 ; i<userColors.length ; i++){
+                loop2: for(int j=0 ; j<codeColors.length ; j++){
+                    if(userColors[i].equals("UX"))
+                        break loop2;
+                    else if(userColors[i].equals(codeColors[j])){
+                        check[checkpos++] = "O";
+                        userColors[i] = "UX";
+                        codeColors[j] = "CX";
+                        break loop2;
+                    }
+                    else if(j==codeColors.length-1){
+                        userColors[i] = "UX";
+                        check[checkpos++]="-";
+                        break loop2;
+                    }
+                }
+            }
+            return check;
+        }
+    }
 
 
