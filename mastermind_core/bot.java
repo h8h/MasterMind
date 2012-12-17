bd2fe1607390e715e26b510985f876615e54f030
h8h <mc2m382@chomeyer.de>
2012-12-17 20:42:40 +0000

    Update Seb's changes

--- mastermind_core/bot.java
+++ mastermind_core/bot.java
@@ -7,19 +7,36 @@ public class bot {
     mm_core = co;
   }
 
-  protected void setBestColors () {
-    for(int i=0; i < mm_core.codeLength(); i++) {
-      if(mm_core.getValueAt(i) == null || mm_core.getValueAt(i).equals("")) {
-        mm_core.setValueAt(mm_core.getEnabledColors()[1],i); //Set for examp red pin to empty pinSetting
-      }
-    }
-    //Do some DEBUG Function Tests
-    if(mm_core.data.size() > 1) {
-    System.out.print("Farbe an Position 1 / 2" + mm_core.getValueAt(1,0));
-    String [] s = mm_core.getHintPane(1);
-      for(int i = 0; i < s.length; i++) {
-        System.out.print(s[i] + " - ");
-      }
-    }
-  }
+	protected void setBestColors () {
+		
+		int space=0;
+	
+		for(int i=0; i < mm_core.codeLength(); i++) {
+			System.out.println(mm_core.getValueAt(i));
+			if(mm_core.getValueAt(i) == null || mm_core.getValueAt(i).equals("")) {
+				space++;
+			}
+		}
+		if (space==mm_core.codeLength()){
+			for(int i=0; i < mm_core.codeLength(); i++) {
+				System.out.println(mm_core.getValueAt(i));
+				if(mm_core.getValueAt(i) == null || mm_core.getValueAt(i).equals("")) {
+					mm_core.setValueAt(mm_core.getEnabledColors()[1],i); //Set for examp red pin to empty pinSetting
+				}
+			}
+			if(mm_core.data.size() > 1) {
+				String [] s = mm_core.getHintPane(1);
+				for(int i = 0; i < s.length; i++) {
+					System.out.print(s[i] + " - ");
+				}
+			}
+		}else{
+			for(int i=0; i < mm_core.codeLength(); i++) {
+				System.out.println(mm_core.getValueAt(i));
+				if(mm_core.getValueAt(i) == null || mm_core.getValueAt(i).equals("")) {
+					mm_core.setValueAt(mm_core.getEnabledColors()[2],i); //Set for examp red pin to empty pinSetting
+				}
+			}
+		}
+	}
 }
---
+++ test.bat
@@ -0,0 +1,2 @@
+javac *.java mastermind_core/*.java mastermind_save_load/*.java mastermind_gui/mastermind_templates/*.java mastermind_gui/*.java
+java main
